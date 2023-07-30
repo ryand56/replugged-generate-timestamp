fe97f26953e7c1c4c1c4dc28c92a933be4c3d6ec
Ryan Omasta <ryand@emeraldsys.xyz>
2023-07-30 15:40:11 -0600

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { Injector, Logger, webpack } from "replugged";
+
+const inject = new Injector();
+const logger = Logger.plugin("GenerateTimestamp");
+
+export async function start(): Promise<void> {}
+
+export function stop(): void {
+  inject.uninjectAll();
+}
